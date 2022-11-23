17d27ee40594c58b9e7541f4386a30e1122aa985
Reece Devenney <reecedevenney@gmail.com>
2022-11-23 00:27:29 -0500

    fixed bug with get route

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,31 +3,32 @@ import { openDB } from 'idb';
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (db.objectStoreNames.contains('words')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('words', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const contactDb = await openDB('jate', 1);
-    const tx = contactDb.transaction('jate', 'readwrite');
-    const store = tx.objectStore('jate');
-    const request= store.add({content})
+  const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('words', 'readwrite');
+    const store = tx.objectStore('words');
+    const request= store.add({content: content})
     const result = await request;
     console.log(result, "added this thing")
 }
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const contactDb = await openDB('jate', 1);
-  const tx = contactDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request= store.getAll()
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('words', 'readonly');
+  const store = tx.objectStore('words');
+  const request= store.getAll();
   const result = await request;
-  return result
+  console.log('result.value', result);
+  return result;
 }
 initdb();
